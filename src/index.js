43057ec2b5d30e526161e52d176c1aa88e416d63
shiva <readytohackk@gmail.com>
2018-05-13 11:17:53 +0530

    added pagination

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/common/Header';
 import List from './components/list/List'
-
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
 import './index.css'
+import Table from './components/list/Table';
+
 const App =()=>{
-  return <div>
+  return <BrowserRouter>
+   <div>
     <Header/>
+<Switch>
+  <Route path='/' component={List} exact/>
 
-    <List/>
+</Switch>
+
   </div>
-
+</BrowserRouter>
 }
 ReactDOM.render(
   <App/>,document.getElementById('root')
